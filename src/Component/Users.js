84366ed6af00e10da9d124e679cf9eff374d62bb
Zahra Teymouri <zahra@macs-MacBook-Air.local>
2020-04-04 14:14:41 +0200

    users improved

--- src/Component/Users.js
+++ src/Component/Users.js
@@ -1,11 +1,43 @@
 import React, {Component} from "react";
+import axios from "axios";
 
 
-class Users extends Component{
+class Users extends Component {
+    constructor() {
+        super();
+        this.state = {
+            message: '',
+            users: '',
+            error: ''
+        }
+
+    }
+
+    componentDidMount() {
+        axios.get('https://arman.co.com/fake-api/users.php')
+            .then(response => {
+                this.setState({
+                    message: response.data.message,
+                    users: response.data.users
+                })
+            })
+            .catch(error => {
+                this.setState({
+                    error: 'There is an error'
+                })
+
+            })
+
+    }
+
+
     render() {
+        let u = this.state.users.length ? this.state.users.map((user)=> <p key={user.id}>{user.name} - {user.age} - {user.isMarried.toString()}</p>) : <p>There is no user</p>
         return (
             <>
-                users
+
+                {this.state.message}
+                {u}
             </>
         );
     }
