90be6c92e4b1932d9d95353d1421a8800775f0a2
Zahra Teymouri <zahra@macs-MacBook-Air.local>
2020-04-04 13:36:35 +0200

    api of products added

--- src/Component/Products.js
+++ src/Component/Products.js
@@ -1,11 +1,43 @@
 import React, {Component} from "react";
+import axios from "axios";
 
 
 class Products extends Component{
+    constructor() {
+        super();
+        this.state = {
+            message: '',
+            products: '',
+            error: ''
+        }
+
+    }
+    componentDidMount() {
+        axios.get('https://arman.co.com/fake-api/products.php')
+            .then(response => {
+                this.setState({
+                    message: response.data.message,
+                    products: response.data.products
+                })
+            })
+            .catch(error => {
+                this.setState({
+                    error: 'There is an error'
+                })
+
+            })
+
+    }
+
+
     render() {
+        const {message, products, error} = this.state
+       let p = products.length ? products.map((product)=> <p>{product.name} - {product.amount} - {product.price}</p>) : <p>There is no products</p>
         return (
             <>
-                products
+
+                {message}
+                {p}
             </>
         );
     }
